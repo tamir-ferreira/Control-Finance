41b607f92b19ba7ccae44b5ba864fe3ba3811c82
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2022-09-28 22:48:31 -0300

    JS em andamento

--- pages/home/valuesData.js
+++ pages/home/valuesData.js
@@ -1,7 +1,7 @@
 const valuesCategory = ["Entrada", "Saída"];
-
-let insertedValues = [
-  {
+const insertedValuesfiltered = [];
+const insertedValues = [
+ /*  {
     id: 1,
     value: 90.0,
     categoryID: 1,
@@ -15,5 +15,5 @@ let insertedValues = [
     id: 3,
     value: 15.5,
     categoryID: 1,
-  },
+  }, */
 ];
